5c446137bed5b53a57f1bd9e38068a4ce86dd4ad
Matthew Lischin <mattboo011@gmail.com>
2020-02-16 07:27:22 -0500

    Tactile Learning Demo

---
+++ Assets/ClickScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnBall()
+    {
+        Instantiate(ballPrefab, new Vector3(1, 2, 2), Quaternion.identity);
+    }
+}
---
+++ Assets/counting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class counting : MonoBehaviour
+{
+    public int numBalls;
+    public TextMeshProUGUI text;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        numBalls++;
+        text.SetText("Balls counted: " + numBalls);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        numBalls--;
+        text.SetText("Balls counted: " + numBalls);
+    }
+
+}
